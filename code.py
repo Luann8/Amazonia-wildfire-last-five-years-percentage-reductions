74642383442191a59c13f01ec5a46ebf673f5fa9
Luann Pereira Mendes <133384636+Luann8@users.noreply.github.com>
2023-12-07 15:24:58 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 
-years = [2019, 2020, 2021, 2022, 2023]
-wildfire_incidents = [34.176, 30.411, 28.641, 22.061, 17.852]
-
+years = [2019, 2020, 2021, 2022, ]
+wildfire_incidents = [34.176, 30.411, 28.641, 22.061]
 percentage_reductions = [-11.5, -6.1, -24.8, -20.7]
 
 fig, ax = plt.subplots()
@@ -14,9 +13,11 @@ plt.title('Wildfire Incidents in the Amazon in the Last 5 Years with Percentage 
 
 for i, rect in enumerate(bars):
     height = rect.get_height()
-    label = f'{height:.1f}%\nReduction'
-    ax.text(rect.get_x() + rect.get_width() / 2, height, label,
-            ha='center', va='bottom', color='black', fontweight='bold')
+    # Check if there's a corresponding percentage reduction for the current year
+    if i < len(percentage_reductions):
+        reduction_label = f'{height:.1f}%\nReduction: {percentage_reductions[i]:.1f}%'
+        ax.text(rect.get_x() + rect.get_width() / 2, height, reduction_label,
+                ha='center', va='bottom', color='black', fontweight='bold')
 
 plt.grid(axis='y')
 plt.show()
